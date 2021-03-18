a78a0575392c5e3062fe87608428a4d86ca01522
Chakravarthy S M <chakrava@synopsys.com>
2021-03-18 12:41:31 +0530

    add missing backslash after coverity password

--- prescription.sh
+++ prescription.sh
@@ -132,9 +132,9 @@ function generateYML () {
 	    s~<<BLACKDUCK_PROJECT_NAME>>~$blackduck_project_name~g; \
 	    s~<<BLACKDUCK_SERVER_URL>>~$blackduck_server_url~g; \
 	    s~<<BLACKDUCK_ACCESS_TOKEN>>~$blackduck_access_token~g; \
-        s~<<COVERITY_SERVER_URL>>~$coverity_server_url~g; \
+	    s~<<COVERITY_SERVER_URL>>~$coverity_server_url~g; \
 	    s~<<COVERITY_USERNAME>>~$coverity_username~g; \
-	    s~<<COVERITY_PASSWORD>>~$coverity_password~g
+	    s~<<COVERITY_PASSWORD>>~$coverity_password~g; \
             s~<<IS_SAST_ENABLED>>~$is_sast_enabled~g; \
             s~<<IS_SCA_ENABLED>>~$is_sca_enabled~g; \
 	    s~<<APP_ID>>~$asset_id~g; \
